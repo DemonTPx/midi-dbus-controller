603dda68224d679ae0cf2ffc055e47883dd217ac
Bert Hekman <demontpx@gmail.com>
2020-04-11 21:05:02 +0200

    Move 7 bit lcd render to separate file Convert display data text to ascii Simplify display data creation

--- src/midi_controller.py
+++ src/midi_controller.py
@@ -1,52 +1,16 @@
 import mido
+from lcd_7bit_font import lcd_7bit_render
+from unidecode import unidecode
 from typing import List
 
 
 class MidiController:
-    letters = {
-        "0": 0b0111111,
-        "1": 0b0000110,
-        "2": 0b1011011,
-        "3": 0b1001111,
-        "4": 0b1100110,
-        "5": 0b1101101,
-        "6": 0b1111101,
-        "7": 0b0000111,
-        "8": 0b1111111,
-        "9": 0b1101111,
-        "A": 0b1110111,
-        "B": 0b1111111,
-        "C": 0b1111111,
-        "D": 0b0111111,
-        "E": 0b1111001,
-        "F": 0b1110001,
-        "G": 0b0111101,
-        "H": 0b1110110,
-        "I": 0b0000110,
-        "J": 0b0001110,
-        "L": 0b0111000,
-        "N": 0b0110111,
-        "O": 0b0111111,
-        "P": 0b1110011,
-        "R": 0b1110111,
-        "S": 0b1101101,
-        "T": 0b1111000,
-        "U": 0b0111110,
-        "V": 0b0111110,
-        "Y": 0b1101110,
-        "Z": 0b1011011,
-        ":": 0b0001001,
-        "-": 0b1000000,
-        ")": 0b0001111,
-        "(": 0b0111001,
-        " ": 0,
-        "=": 0b1001001,
-        "|": 0b0110110,
-    }
-
     def __init__(self, name: str) -> None:
         self._name = name
         self._port_out = mido.open_output(name)
+
+    def open_input(self, callback: callable):
+        return mido.open_input(self._name, callback=callback)
 
     def reset(self) -> None:
         for n in range(1, 35):
@@ -71,31 +35,22 @@ class MidiController:
     def sysex(self, data: List[int]) -> None:
         self._send(mido.Message('sysex', data=data))
 
-    def open_input(self, callback: callable):
-        return mido.open_input(self._name, callback=callback)
+    def _send(self, message: mido.Message) -> None:
+        self._port_out.send(message)
 
     def create_lcd_display_data(self, characters: str, cc: int) -> List[int]:
-        character_data = []
-        for n in range(0, 14):
-            if n < len(characters):
-                character_data.append(ord(characters[n]))
-            else:
-                character_data.append(0)
+        characters = unidecode(characters)
+        character_data = self._pad_to(list(map(ord, characters[:14])), 14)
 
         return [0x00, 0x20, 0x32, 0x41, 0x4c, 0x00, cc] + character_data
 
     def create_segment_display_data(self, characters: str) -> List[int]:
-        if len(characters) > 12:
-            raise
-
-        character_data = []
-        for n in range(0, 12):
-            if n < len(characters):
-                character_data.append(self.letters[characters[n]])
-            else:
-                character_data.append(0x00)
+        characters = unidecode(characters)
+        character_data = self._pad_to(lcd_7bit_render(characters[:12]), 12)
 
         return [0x00, 0x20, 0x32, 0x41, 0x37] + character_data + [0x00, 0x00]
 
-    def _send(self, message: mido.Message) -> None:
-        self._port_out.send(message)
+    @staticmethod
+    def _pad_to(data: List[int], n: int) -> List[int]:
+        trimmed = data[:n]
+        return trimmed + [0] * (n - len(trimmed))
